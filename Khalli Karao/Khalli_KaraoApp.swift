3b3a1062f636c100b097300a4303811ccababc49
maaztausif <maaztausif48@gmail.com>
2026-05-31 21:16:21 +0200

    Initial Commit

---
+++ Khalli Karao/Khalli_KaraoApp.swift
@@ -0,0 +1,17 @@
+//
+//  Khalli_KaraoApp.swift
+//  Khalli Karao
+//
+//  Created by Maaz Bin Tausif on 31.05.26.
+//
+
+import SwiftUI
+
+@main
+struct Khalli_KaraoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Khalli KaraoTests/Khalli_KaraoTests.swift
@@ -0,0 +1,16 @@
+//
+//  Khalli_KaraoTests.swift
+//  Khalli KaraoTests
+//
+//  Created by Maaz Bin Tausif on 31.05.26.
+//
+
+import Testing
+
+struct Khalli_KaraoTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
